a486e5f659ebdcc0df8cde07b2d6b6078cca3684
kalpakavindu <kalpadevonline@gmail.com>
2024-10-09 23:24:25 +0530

    feature(MainScreen): MainScreen UI completed

--- Skills/Screens/LoginScreen.Designer.cs
+++ Skills/Screens/LoginScreen.Designer.cs
@@ -49,9 +49,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.AppLogo.BackgroundImage = global::Skills.Properties.Resources.app_logo;
             this.AppLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.AppLogo.Location = new System.Drawing.Point(205, 22);
+            this.AppLogo.Location = new System.Drawing.Point(182, 18);
+            this.AppLogo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.AppLogo.Name = "AppLogo";
-            this.AppLogo.Size = new System.Drawing.Size(135, 105);
+            this.AppLogo.Size = new System.Drawing.Size(120, 84);
             this.AppLogo.TabIndex = 0;
             this.AppLogo.TabStop = false;
             // 
@@ -60,9 +61,9 @@
             this.AppName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.AppName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AppName.Location = new System.Drawing.Point(146, 130);
+            this.AppName.Location = new System.Drawing.Point(104, 104);
             this.AppName.Name = "AppName";
-            this.AppName.Size = new System.Drawing.Size(255, 31);
+            this.AppName.Size = new System.Drawing.Size(273, 37);
             this.AppName.TabIndex = 1;
             this.AppName.Text = "Skills International";
             this.AppName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -77,9 +78,11 @@
             this.LoginFormWrapper.Controls.Add(this.LabelPassword);
             this.LoginFormWrapper.Controls.Add(this.InputUsername);
             this.LoginFormWrapper.Controls.Add(this.LabelUsername);
-            this.LoginFormWrapper.Location = new System.Drawing.Point(80, 179);
+            this.LoginFormWrapper.Location = new System.Drawing.Point(71, 143);
+            this.LoginFormWrapper.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.LoginFormWrapper.Name = "LoginFormWrapper";
-            this.LoginFormWrapper.Size = new System.Drawing.Size(374, 231);
+            this.LoginFormWrapper.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.LoginFormWrapper.Size = new System.Drawing.Size(332, 185);
             this.LoginFormWrapper.TabIndex = 2;
             this.LoginFormWrapper.TabStop = false;
             this.LoginFormWrapper.Text = "Login";
@@ -87,18 +90,20 @@
             // BtnSubmit
             // 
             this.BtnSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.BtnSubmit.Location = new System.Drawing.Point(261, 167);
+            this.BtnSubmit.Location = new System.Drawing.Point(232, 134);
+            this.BtnSubmit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnSubmit.Name = "BtnSubmit";
-            this.BtnSubmit.Size = new System.Drawing.Size(89, 34);
+            this.BtnSubmit.Size = new System.Drawing.Size(79, 27);
             this.BtnSubmit.TabIndex = 5;
             this.BtnSubmit.Text = "Login";
             this.BtnSubmit.UseVisualStyleBackColor = true;
             // 
             // BtnClear
             // 
-            this.BtnClear.Location = new System.Drawing.Point(27, 167);
+            this.BtnClear.Location = new System.Drawing.Point(24, 134);
+            this.BtnClear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnClear.Name = "BtnClear";
-            this.BtnClear.Size = new System.Drawing.Size(89, 34);
+            this.BtnClear.Size = new System.Drawing.Size(79, 27);
             this.BtnClear.TabIndex = 4;
             this.BtnClear.Text = "Clear";
             this.BtnClear.UseVisualStyleBackColor = true;
@@ -108,18 +113,19 @@
             this.InputPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.InputPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.InputPassword.Location = new System.Drawing.Point(134, 108);
+            this.InputPassword.Location = new System.Drawing.Point(119, 86);
+            this.InputPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.InputPassword.MaxLength = 50;
             this.InputPassword.Name = "InputPassword";
-            this.InputPassword.Size = new System.Drawing.Size(216, 26);
+            this.InputPassword.Size = new System.Drawing.Size(192, 22);
             this.InputPassword.TabIndex = 3;
             // 
             // LabelPassword
             // 
             this.LabelPassword.AutoSize = true;
-            this.LabelPassword.Location = new System.Drawing.Point(23, 110);
+            this.LabelPassword.Location = new System.Drawing.Point(20, 88);
             this.LabelPassword.Name = "LabelPassword";
-            this.LabelPassword.Size = new System.Drawing.Size(78, 20);
+            this.LabelPassword.Size = new System.Drawing.Size(67, 16);
             this.LabelPassword.TabIndex = 2;
             this.LabelPassword.Text = "Password";
             // 
@@ -128,45 +134,47 @@
             this.InputUsername.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.InputUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.InputUsername.Location = new System.Drawing.Point(134, 50);
+            this.InputUsername.Location = new System.Drawing.Point(119, 40);
+            this.InputUsername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.InputUsername.MaxLength = 50;
             this.InputUsername.Name = "InputUsername";
-            this.InputUsername.Size = new System.Drawing.Size(216, 26);
+            this.InputUsername.Size = new System.Drawing.Size(192, 22);
             this.InputUsername.TabIndex = 1;
             // 
             // LabelUsername
             // 
             this.LabelUsername.AutoSize = true;
-            this.LabelUsername.Location = new System.Drawing.Point(23, 53);
+            this.LabelUsername.Location = new System.Drawing.Point(20, 42);
             this.LabelUsername.Name = "LabelUsername";
-            this.LabelUsername.Size = new System.Drawing.Size(83, 20);
+            this.LabelUsername.Size = new System.Drawing.Size(70, 16);
             this.LabelUsername.TabIndex = 0;
             this.LabelUsername.Text = "Username";
             // 
             // BtnExit
             // 
             this.BtnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.BtnExit.Location = new System.Drawing.Point(12, 437);
+            this.BtnExit.Location = new System.Drawing.Point(11, 350);
+            this.BtnExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnExit.Name = "BtnExit";
-            this.BtnExit.Size = new System.Drawing.Size(89, 34);
+            this.BtnExit.Size = new System.Drawing.Size(79, 27);
             this.BtnExit.TabIndex = 6;
             this.BtnExit.Text = "Exit";
             this.BtnExit.UseVisualStyleBackColor = true;
             // 
             // LoginScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(545, 483);
+            this.ClientSize = new System.Drawing.Size(484, 386);
             this.Controls.Add(this.BtnExit);
             this.Controls.Add(this.LoginFormWrapper);
             this.Controls.Add(this.AppName);
             this.Controls.Add(this.AppLogo);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.MinimumSize = new System.Drawing.Size(561, 522);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MinimumSize = new System.Drawing.Size(500, 425);
             this.Name = "LoginScreen";
             this.Text = "Login - Skills International";
             ((System.ComponentModel.ISupportInitialize)(this.AppLogo)).EndInit();
---
+++ Skills/Screens/MainScreen.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Skills.Screens
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
+            this.NavigationWrapper = new System.Windows.Forms.Panel();
+            this.ContentPanel = new System.Windows.Forms.Panel();
+            this.AppName = new System.Windows.Forms.Label();
+            this.BtnHome = new System.Windows.Forms.Button();
+            this.BtnStudentManagement = new System.Windows.Forms.Button();
+            this.BtnTeacherManagement = new System.Windows.Forms.Button();
+            this.BtnClassManagement = new System.Windows.Forms.Button();
+            this.BtnLogout = new System.Windows.Forms.Button();
+            this.NavigationWrapper.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NavigationWrapper
+            // 
+            this.NavigationWrapper.BackColor = System.Drawing.Color.Lavender;
+            this.NavigationWrapper.Controls.Add(this.BtnLogout);
+            this.NavigationWrapper.Controls.Add(this.BtnClassManagement);
+            this.NavigationWrapper.Controls.Add(this.BtnTeacherManagement);
+            this.NavigationWrapper.Controls.Add(this.BtnStudentManagement);
+            this.NavigationWrapper.Controls.Add(this.BtnHome);
+            this.NavigationWrapper.Controls.Add(this.AppName);
+            this.NavigationWrapper.Dock = System.Windows.Forms.DockStyle.Left;
+            this.NavigationWrapper.Location = new System.Drawing.Point(0, 0);
+            this.NavigationWrapper.Name = "NavigationWrapper";
+            this.NavigationWrapper.Size = new System.Drawing.Size(234, 551);
+            this.NavigationWrapper.TabIndex = 0;
+            // 
+            // ContentPanel
+            // 
+            this.ContentPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ContentPanel.BackColor = System.Drawing.SystemColors.Control;
+            this.ContentPanel.Location = new System.Drawing.Point(240, 0);
+            this.ContentPanel.Name = "ContentPanel";
+            this.ContentPanel.Size = new System.Drawing.Size(827, 551);
+            this.ContentPanel.TabIndex = 1;
+            // 
+            // AppName
+            // 
+            this.AppName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AppName.AutoSize = true;
+            this.AppName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AppName.Location = new System.Drawing.Point(13, 9);
+            this.AppName.Name = "AppName";
+            this.AppName.Size = new System.Drawing.Size(207, 25);
+            this.AppName.TabIndex = 0;
+            this.AppName.Text = "Skills International";
+            this.AppName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnHome
+            // 
+            this.BtnHome.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnHome.BackColor = System.Drawing.SystemColors.Control;
+            this.BtnHome.FlatAppearance.BorderSize = 0;
+            this.BtnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnHome.Location = new System.Drawing.Point(3, 52);
+            this.BtnHome.Name = "BtnHome";
+            this.BtnHome.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.BtnHome.Size = new System.Drawing.Size(231, 34);
+            this.BtnHome.TabIndex = 1;
+            this.BtnHome.Text = "Home";
+            this.BtnHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnHome.UseVisualStyleBackColor = false;
+            // 
+            // BtnStudentManagement
+            // 
+            this.BtnStudentManagement.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnStudentManagement.BackColor = System.Drawing.Color.Lavender;
+            this.BtnStudentManagement.FlatAppearance.BorderSize = 0;
+            this.BtnStudentManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnStudentManagement.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStudentManagement.Location = new System.Drawing.Point(3, 92);
+            this.BtnStudentManagement.Name = "BtnStudentManagement";
+            this.BtnStudentManagement.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.BtnStudentManagement.Size = new System.Drawing.Size(231, 34);
+            this.BtnStudentManagement.TabIndex = 2;
+            this.BtnStudentManagement.Text = "Student Management";
+            this.BtnStudentManagement.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnStudentManagement.UseVisualStyleBackColor = false;
+            // 
+            // BtnTeacherManagement
+            // 
+            this.BtnTeacherManagement.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnTeacherManagement.BackColor = System.Drawing.Color.Lavender;
+            this.BtnTeacherManagement.FlatAppearance.BorderSize = 0;
+            this.BtnTeacherManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnTeacherManagement.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTeacherManagement.Location = new System.Drawing.Point(3, 132);
+            this.BtnTeacherManagement.Name = "BtnTeacherManagement";
+            this.BtnTeacherManagement.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.BtnTeacherManagement.Size = new System.Drawing.Size(231, 34);
+            this.BtnTeacherManagement.TabIndex = 3;
+            this.BtnTeacherManagement.Text = "Teacher Management";
+            this.BtnTeacherManagement.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnTeacherManagement.UseVisualStyleBackColor = false;
+            // 
+            // BtnClassManagement
+            // 
+            this.BtnClassManagement.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnClassManagement.BackColor = System.Drawing.Color.Lavender;
+            this.BtnClassManagement.FlatAppearance.BorderSize = 0;
+            this.BtnClassManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnClassManagement.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnClassManagement.Location = new System.Drawing.Point(3, 172);
+            this.BtnClassManagement.Name = "BtnClassManagement";
+            this.BtnClassManagement.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.BtnClassManagement.Size = new System.Drawing.Size(231, 34);
+            this.BtnClassManagement.TabIndex = 4;
+            this.BtnClassManagement.Text = "Class Management";
+            this.BtnClassManagement.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnClassManagement.UseVisualStyleBackColor = false;
+            // 
+            // BtnLogout
+            // 
+            this.BtnLogout.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnLogout.BackColor = System.Drawing.Color.Lavender;
+            this.BtnLogout.FlatAppearance.BorderSize = 0;
+            this.BtnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLogout.Location = new System.Drawing.Point(3, 514);
+            this.BtnLogout.Name = "BtnLogout";
+            this.BtnLogout.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.BtnLogout.Size = new System.Drawing.Size(231, 34);
+            this.BtnLogout.TabIndex = 5;
+            this.BtnLogout.Text = "Logout";
+            this.BtnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnLogout.UseVisualStyleBackColor = false;
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 551);
+            this.Controls.Add(this.ContentPanel);
+            this.Controls.Add(this.NavigationWrapper);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainScreen";
+            this.Text = "Dashboard - Skills International";
+            this.NavigationWrapper.ResumeLayout(false);
+            this.NavigationWrapper.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel NavigationWrapper;
+        private System.Windows.Forms.Panel ContentPanel;
+        private System.Windows.Forms.Label AppName;
+        private System.Windows.Forms.Button BtnClassManagement;
+        private System.Windows.Forms.Button BtnTeacherManagement;
+        private System.Windows.Forms.Button BtnStudentManagement;
+        private System.Windows.Forms.Button BtnHome;
+        private System.Windows.Forms.Button BtnLogout;
+    }
+}
